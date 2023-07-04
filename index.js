c746955f810ef56d75b50490691436cc73cb96f3
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-04 09:53:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+};
+function reverseString(word) {
+  return word.split("").reverse().join("");
+};
 
 /* 
   Add your pseudocode here
